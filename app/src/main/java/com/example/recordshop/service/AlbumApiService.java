f4caa5cbe75091634362be70890f47f18dc80351
kadri-k <kklampe@gmail.com>
2025-01-05 18:23:37 +0000

    feat: create method for GET request in AlbumApiService interface

--- app/src/main/java/com/example/recordshop/service/AlbumApiService.java
+++ app/src/main/java/com/example/recordshop/service/AlbumApiService.java
@@ -1,4 +1,14 @@
 package com.example.recordshop.service;
 
+import com.example.recordshop.model.Album;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface AlbumApiService {
+
+    @GET("albums")
+    Call<List<Album>> getAllAlbums();
 }
